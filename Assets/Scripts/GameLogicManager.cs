2a7cf94822ba810dcefb5090f3bcd58573ed9372
DraiRuben <r.drai@lahorde.school>
2023-04-18 11:01:16 +0200

    boo

--- Assets/Scripts/GameLogicManager.cs
+++ Assets/Scripts/GameLogicManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +10,7 @@ public class GameLogicManager : MonoBehaviour
 
     private bool isInQTE = false;
     private bool isInPnC = false;
+    private bool HasDefault = false;
 
     private Coroutine Action;
     private KeyInputs KeyInputs;
@@ -17,6 +19,7 @@ public class GameLogicManager : MonoBehaviour
     private float ActionDuration = 0f;
     private float ActionTime = 0f;
     private double ActionEnd = 0f;
+    private double ActionStart = 0f;
 
     // Start is called before the first frame update
 
@@ -30,7 +33,7 @@ public class GameLogicManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Video.instance?.GetComponent<Video>().CheckAction();
+        Video.instance?.GetComponent<Video>()?.CheckAction();
         if (isInQTE && Action == null)
         {
             Action = StartCoroutine("QTE_Routine");
@@ -40,7 +43,7 @@ public class GameLogicManager : MonoBehaviour
             Action = StartCoroutine("PnC_Routine");
         }
     }
-    public void RequestQTE(float duration, double EndTime, KeyInputs keys, GameObject defaultScene)
+    public void RequestQTE(float duration, double EndTime, double StartTime,KeyInputs keys, GameObject defaultScene, bool hasDefault)
     {
         ActionTime = 0f;
         ActionDuration = duration;
@@ -48,24 +51,29 @@ public class GameLogicManager : MonoBehaviour
         isInPnC = false;
         KeyInputs = keys;
         ActionEnd = EndTime;
+        ActionStart = StartTime;
         defaultVideo = defaultScene;
+        HasDefault = hasDefault;
 
     }
 
-    public void RequestPnC(float duration, List<TouchInputs> Touch, GameObject defaultScene)
+    public void RequestPnC(float duration, double StartTime, List<TouchInputs> Touch, GameObject defaultScene, bool hasDefault)
     {
         ActionTime = 0f;
         ActionDuration = duration;
         isInQTE = false;
         isInPnC = true;
         TouchInputs = Touch;
+        ActionStart = StartTime;
         defaultVideo = defaultScene;
+        HasDefault = hasDefault;
+
     }
 
     private IEnumerator QTE_Routine()
     {
         //finds correct speed so that when the QTE ends, it ends at the target time in the video
-        Video.ChangeSpeed((float)(ActionDuration / (ActionEnd - Video.GetCurrentTime())));
+        Video.ChangeSpeed((float)(1/(ActionDuration / (ActionEnd - ActionStart))));
         while (isInQTE)
         {
             ActionTime += Time.deltaTime;
@@ -87,10 +95,15 @@ public class GameLogicManager : MonoBehaviour
             else if (ActionTime >= ActionDuration)
             {
                 //Do thing if the QTE was a failure then exits coroutine by breaking out of the while loop
-                Destroy(Video.instance);
-                Video.instance = Instantiate(defaultVideo);
+                if (HasDefault)
+                {
+                    Destroy(Video.instance);
+                    Video.instance = Instantiate(defaultVideo);
+                }
                 break;
             }
+            yield return null;
+
         }
         //exits the coroutine
         Video.ChangeSpeed(1);
@@ -106,34 +119,38 @@ public class GameLogicManager : MonoBehaviour
             ActionTime += Time.deltaTime;
             if (Input.GetMouseButtonDown(0))
             {
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
-                if (Physics.Raycast(ray, out hit))
+                Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
+                if (hit.collider != null)
                 {
-                    for(int i = 0; i < TouchInputs.Count; i++)
+                    for (int i = 0; i < TouchInputs.Count; i++)
                     {
                         if (hit.transform.gameObject == TouchInputs[i].button.gameObject)
                         {
                             //do thing if hitbox is clicked
-                            if(TouchInputs[i].isLeading)
+                            if (TouchInputs[i].isLeading)
                             {
-                                Debug.Log("clicked");
+                                
                                 Destroy(Video.instance);
                                 Video.instance = Instantiate(TouchInputs[i].prefab);
                             }
                             goto ext;
                         }
                     }
-                    
                 }
             }
             else if (ActionTime >= ActionDuration)
             {
                 //Do thing if the PnC was a failure then exits coroutine by breaking out of the while loop
-                Destroy(Video.instance);
-                Video.instance = Instantiate(defaultVideo);
+                if (HasDefault)
+                {
+                    Destroy(Video.instance);
+                    Video.instance = Instantiate(defaultVideo);
+                }
+
                 break;
             }
+            yield return null;
             
         }
         //exits the coroutine
